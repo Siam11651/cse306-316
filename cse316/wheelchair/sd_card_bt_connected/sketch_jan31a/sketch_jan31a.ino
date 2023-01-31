313523bfd93446fafc410a8c5d05f77d125fb0ea
Mohaimin41 <1905041@ugrad.cse.buet.ac.bd>
2023-01-31 12:43:54 +0600

    json_on_bt

---
+++ cse316/wheelchair/sd_card_bt_connected/sketch_jan31a/sketch_jan31a.ino
@@ -0,0 +1,239 @@
+#include <SPI.h>
+#include <SoftwareSerial.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+#include <SD.h>
+
+Adafruit_MPU6050 mpu;
+sensors_event_t a, g, temp;
+int acceleration;
+
+FILE file;
+
+
+SoftwareSerial sSerial(4, 5);
+
+#define X_AXIS_PIN A1  // left-right
+#define Y_AXIS_PIN A0  // front-back
+#define SW_PIN 10
+
+// motor left
+#define IN_LEFT1 6  // motor A spin 1
+#define IN_LEFT2 7  // motor A spin 2
+
+// motor right
+#define IN_RIGHT1 8  // motor B spin 1
+#define IN_RIGHT2 9  // motor B spin 2
+
+// speed factor
+#define FACTOR 0.3  // average fraction of total voltage (PWM)
+
+// loop delay
+#define DELTA 10.0  // ms
+#define HORIZONTAL_THRESHOLD 400
+#define VERTICAL_THRESHOLD 100
+
+//bluetooh items
+
+bool bt_flag;
+String command = "";
+String state = "";
+bool onInput;
+bool stateCheckedByApp = false;
+
+void Forward() {
+  digitalWrite(IN_LEFT1, HIGH);
+  digitalWrite(IN_LEFT2, LOW);
+  digitalWrite(IN_RIGHT1, LOW);
+  digitalWrite(IN_RIGHT2, HIGH);
+  delay(DELTA * FACTOR);
+  digitalWrite(IN_LEFT1, LOW);
+  digitalWrite(IN_LEFT2, LOW);
+  digitalWrite(IN_RIGHT1, LOW);
+  digitalWrite(IN_RIGHT2, LOW);
+  delay(DELTA * (1.0 - FACTOR));
+}
+
+void Backward() {
+  digitalWrite(IN_LEFT1, LOW);
+  digitalWrite(IN_LEFT2, HIGH);
+  digitalWrite(IN_RIGHT1, HIGH);
+  digitalWrite(IN_RIGHT2, LOW);
+  delay(DELTA * FACTOR);
+  digitalWrite(IN_LEFT1, LOW);
+  digitalWrite(IN_LEFT2, LOW);
+  digitalWrite(IN_RIGHT1, LOW);
+  digitalWrite(IN_RIGHT2, LOW);
+  delay(DELTA * (1.0 - FACTOR));
+}
+
+void Left() {
+  digitalWrite(IN_LEFT1, LOW);
+  digitalWrite(IN_LEFT2, HIGH);
+  digitalWrite(IN_RIGHT1, LOW);
+  digitalWrite(IN_RIGHT2, HIGH);
+  delay(DELTA * FACTOR);
+  digitalWrite(IN_LEFT1, LOW);
+  digitalWrite(IN_LEFT2, LOW);
+  digitalWrite(IN_RIGHT1, LOW);
+  digitalWrite(IN_RIGHT2, LOW);
+  delay(DELTA * (1.0 - FACTOR));
+}
+
+void Right() {
+  digitalWrite(IN_LEFT1, HIGH);
+  digitalWrite(IN_LEFT2, LOW);
+  digitalWrite(IN_RIGHT1, HIGH);
+  digitalWrite(IN_RIGHT2, LOW);
+  delay(DELTA * FACTOR);
+  digitalWrite(IN_LEFT1, LOW);
+  digitalWrite(IN_LEFT2, LOW);
+  digitalWrite(IN_RIGHT1, LOW);
+  digitalWrite(IN_RIGHT2, LOW);
+  delay(DELTA * (1.0 - FACTOR));
+}
+
+void Stop() {
+  digitalWrite(IN_LEFT1, LOW);
+  digitalWrite(IN_LEFT2, LOW);
+  digitalWrite(IN_RIGHT1, LOW);
+  digitalWrite(IN_RIGHT2, LOW);
+  delay(DELTA);
+}
+
+void setup() {
+  // Serial.begin(9600);
+  sSerial.begin(9600);
+  pinMode(SW_PIN, INPUT);
+  digitalWrite(SW_PIN, HIGH);
+  pinMode(X_AXIS_PIN, INPUT);
+  pinMode(Y_AXIS_PIN, INPUT);
+  pinMode(IN_LEFT1, OUTPUT);
+  pinMode(IN_LEFT2, OUTPUT);
+  pinMode(IN_RIGHT1, OUTPUT);
+  pinMode(IN_RIGHT2, OUTPUT);
+
+  Serial.begin(115200);
+
+  // Try to initialize!
+  if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+    }
+  }
+  // Serial.println("MPU6050 Found!");
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(12, OUTPUT);
+  F
+    // set accelerometer range to +-8G
+    mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+
+  // set gyro range to +- 500 deg/s
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+
+  // set filter bandwidth to 21 Hz
+  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+
+  if (!SD.begin()) {
+    Serial.println("Error: SD not initialized");
+  }
+
+  file = SD.open("number.txt", FILE_WRITE | FILE_READ);
+
+  delay(100);
+}
+
+void loop() {
+  mpu.getEvent(&a, &g, &temp);
+
+  acceleration = a.acceleration.x * a.acceleration.x + a.acceleration.y * a.acceleration.y + a.acceleration.z * a.acceleration.z;
+  Serial.print(acceleration);
+  Serial.print(",");
+  if (acceleration > 130 || acceleration < 70) {
+    digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+    digitalWrite(12, HIGH);
+  }
+
+  int xAxisInput = analogRead(X_AXIS_PIN);
+  int yAxisInput = analogRead(Y_AXIS_PIN);
+  int joystickinput = digitalRead(SW_PIN);
+  for (int i = 0; i < sSerial.available(); i++) {
+
+    char c = sSerial.read();
+    onInput = true;
+    if (c != '\\') {
+      if ('a' <= c && c <= 'z' || c == '$') {
+        command += c;
+      }
+    } else {
+      if (command == "$") {
+        state = "";
+      } else {
+        state = command;
+      }
+      onInput = false;
+      break;
+    }
+  }
+  if (state == "x") {
+    bt_flag = true;
+  } else if (state == "y") {
+    bt_flag = false;
+  } else if (state == "q") {
+    //bt flag on== 1 or off == 0
+    if (bt_flag) {
+      sSerial.write("1");  //
+    } else {
+      // sSerial.print("0");
+      sSerial.write("0");
+    }
+    state = "";
+    
+  } else if (state[0] == 's') { // checking if first char of state is 's', what follows shall be the 
+    if (file) {
+      file.println(command);
+      file.close();
+    }
+  }
+
+  if (bt_flag) {
+    if (state == "f") {
+      Forward();
+    } else if (state == "b") {
+      Backward();
+    } else if (state == "r") {
+      Right();
+    } else if (state == "l") {
+      Left();
+    }
+  } else {
+    if (VERTICAL_THRESHOLD < yAxisInput && yAxisInput < 1023 - VERTICAL_THRESHOLD)  // if y axis not in threshold move left right
+    {
+      if (HORIZONTAL_THRESHOLD < xAxisInput && xAxisInput < 1023 - HORIZONTAL_THRESHOLD) {
+        Stop();
+      } else {
+        if (xAxisInput < HORIZONTAL_THRESHOLD) {
+          Left();
+        } else {
+          Right();
+        }
+      }
+    } else {
+      if (yAxisInput < VERTICAL_THRESHOLD) {
+
+        Forward();
+      } else {
+
+        Backward();
+      }
+    }
+  }
+  if (!onInput) {
+    command = "";
+  }
+  digitalWrite(LED_BUILTIN, LOW);
+  Serial.println("");
+}
