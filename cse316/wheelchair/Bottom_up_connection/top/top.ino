72d26fb7a5f63f55167905f8aa24e0b3d7e26a56
Sadif Ahmed <1905058@ugrad.cse.buet.ac.bd>
2023-02-05 23:11:32 +0600

    Added Keypad Input

--- cse316/wheelchair/Bottom_up_connection/top/top.ino
+++ cse316/wheelchair/Bottom_up_connection/top/top.ino
@@ -1,11 +1,28 @@
 #include<SoftwareSerial.h>
 #include<EEPROM.h>
 #include <LiquidCrystal_I2C.h>
+#include <Keypad.h>
 LiquidCrystal_I2C lcd(0x3F,16,2);  // set the LCD address to 0x3F for a 16 chars and 2 line display
-String num="01521564856"; //Initialized variable to store receive
+const byte ROWS = 4; //four rows
+const byte COLS = 3; //three columns
+String num; //Initialized variable to store receive
+String tempnum;
 String state;
 const int numberStartAddress=0;
-SoftwareSerial dSerial(3, 2);
+bool numinputstart;
+SoftwareSerial dSerial(11, 12);
+
+char keys[ROWS][COLS] = {
+  {'1','2','3'},
+  {'4','5','6'},
+  {'7','8','9'},
+  {'*','0','#'}
+};
+byte rowPins[ROWS] = {10, 9, 8, 7}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {6, 5, 4}; //connect to the column pinouts of the keypad
+
+//Create an object of keypad
+Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 void set_num()
 {
     for (int offset = 0; offset < 11; offset++) {
@@ -19,6 +36,22 @@ void read_num()
       num += EEPROM.read(numberStartAddress + offset);
     }
 }
+bool number_validation()
+{
+  if(tempnum.length()!=11)
+  {
+    return false;
+  }
+  for(int i=0;i<11;i++)
+  {
+    if(tempnum[i]=='#' || tempnum[i] == '*')
+    {
+      return false;
+    }
+  }
+  return true;
+  
+}
 void setup() {
  Serial.begin(38400); 
  dSerial.begin(19200); // Arduino Connection Band
@@ -29,6 +62,36 @@ void setup() {
  
 }
 void loop() {
+  char key = keypad.getKey();// Read the key
+  if (key){
+    if(key == '*')
+    {
+      if(numinputstart==false)
+      {
+         numinputstart=true;
+         tempnum = "";    
+      }
+     
+    }
+    else if(key == '#')
+    {
+      if(numinputstart ==  true)
+      {
+         numinputstart=false;
+      if(number_validation())
+      {
+        num=tempnum;
+      }    
+      }
+    }
+    else
+    {
+      if(numinputstart ==  true)
+      {
+        tempnum+=key;
+      }
+    }
+  }
   for(int i=0;i<dSerial.available();i++)
   {
     char x =  dSerial.read();
