9e9dc256f7f3d1b727606b2a3fb7716e8fb716af
Siam11651 <siam11651@outlook.com>
2023-02-24 14:59:01 +0600

    kaaj kori

--- cse306/offline-3/atmega_tests/InstructionMemory/InstructionMemory/main.c
+++ cse306/offline-3/atmega_tests/InstructionMemory/InstructionMemory/main.c
@@ -9,33 +9,52 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-uint16_t INSTRUCTIONS[1<<8] = {
-	0x206f, 0x2013, 0x702e, 0xe127, 0x6123, 0xb724, 0x5213, 0xf221, 0x8321, 0xa1c0, 0x1331, 0xd224, 0x5610, 0x7661, 0x5620, 0x7661, 0xa1a0, 0x4727, 0x9421, 0x3115, 0x2661, 0xd620, 0x0241, 0x2661, 0xd610, 0xa1c0, 0x8271, 0xa110
+uint16_t INSTRUCTIONS[1<<8] =
+{
+	306f,
+	3012,
+	3023,
+	6123
 };
+
+uint16_t instructionCount = 4;
 
 int main(void)
 {
 	
 	// setting up input output mode
-    DDRA = 0b00000000;	// input pc
+    DDRA = 0b00000000;	// instruction count write
 	
 	// Instruction : D[7:0]B[7:0]
 	DDRB = 0b11111111;	// lower 8-bits of instruction
 	DDRD = 0b11111111;	// upper 8-bits of instruction
+	DDRC = 0b11111111; // instruction count read
 	
-	uint8_t pc = -1;
+	// uint8_t pc = -1;
 	uint16_t instruction;
+
+	for(uint16_t pc = 0; pc < instructionCount; ++pc)
+	{
+		PORTC = pc;
+		instruction = INSTRUCTIONS[pc];
+		PORTB = (uint8_t) instruction;
+		PORTD = (uint8_t) (instruction >> 8); 
+		pc = PINA;
+		_delay_ms(50);
+	}
+
+	while(1); // so it does not die
 	
-	while (1) 
-    {
-		if (PINA != pc) {	// checking if pc is same is previous, would be a mistake
-			pc = PINA;
-			instruction = INSTRUCTIONS[pc];
-			PORTB = (uint8_t) instruction;
-			PORTD = (uint8_t) (instruction >> 8); 
-			_delay_ms(50);
-		}
-    }
+	// while (1) 
+    // {
+	// 	if (PINA != pc) {	// checking if pc is same is previous, would be a mistake
+	// 		pc = PINA;
+	// 		instruction = INSTRUCTIONS[pc];
+	// 		PORTB = (uint8_t) instruction;
+	// 		PORTD = (uint8_t) (instruction >> 8); 
+	// 		_delay_ms(50);
+	// 	}
+    // }
 }
 
 
