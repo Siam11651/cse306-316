a45f3e3dadcca8e2a94108b82beae775ce14015e
Sadif Ahmed <1905058@ugrad.cse.buet.ac.bd>
2023-02-26 01:03:05 +0600

    Finished

--- cse316/wheelchair/Final Codes/top/top.ino
+++ cse316/wheelchair/Final Codes/top/top.ino
@@ -1,3 +1,5 @@
+#define USE_ARDUINO_INTERRUPTS true    // Set-up low-level interrupts for most acurate BPM math.
+#include <PulseSensorPlayground.h>     // Includes the PulseSensorPlayground Library.   
 #include <LiquidCrystal_I2C.h>
 #include<SoftwareSerial.h>
 #include<EEPROM.h>
@@ -12,6 +14,15 @@ bool started = false;
 int message_sent=0;
 SoftwareSerial dSerial(11, 12);
 SoftwareSerial sSerial(2, 3);
+const int PulseWire = 0;       // PulseSensor PURPLE WIRE connected to ANALOG PIN 0
+const int LED = LED_BUILTIN;          // The on-board Arduino LED, close to PIN 13.
+int Threshold = 550;           // Determine which Signal to "count as a beat" and which to ignore.
+                               // Use the "Gettting Started Project" to fine-tune Threshold Value beyond default setting.
+                               // Otherwise leave the default "550" value. 
+                               
+PulseSensorPlayground pulseSensor;  // Creates an instance of the PulseSensorPlayground object called "pulseSensor"
+
+
 void set_num()
 {
     for (int offset = 0; offset < 11; offset++) {
@@ -40,17 +51,42 @@ void SendMessage(String number, String message)
 void setup() {
   lcd.begin();
   lcd.clear();         
-  lcd.backlight();      // Make sure backlight is on
+  lcd.backlight();
+  // Make sure backlight is on
   // Print a message on both lines of the LCD.
   Serial.begin(9600);
   sSerial.begin(4800);
   dSerial.begin(19200);
-  
-  
+  read_num();  
+  // Configure the PulseSensor object, by assigning our variables to it. 
+  pulseSensor.analogInput(PulseWire);   
+  pulseSensor.blinkOnPulse(LED);       //auto-magically blink Arduino's LED with heartbeat.
+  pulseSensor.setThreshold(Threshold);   
+
+  // Double-check the "pulseSensor" object was created and "began" seeing a signal. 
+   if (pulseSensor.begin()) {
+    Serial.println("We created a pulseSensor Object !");  //This prints one time at Arduino power-up,  or on Arduino reset.  
+  }
 }
 
 void loop() {
-  
+  int mybpm;
+  if (pulseSensor.sawStartOfBeat()) {            // Constantly test to see if "a beat happened".
+mybpm = pulseSensor.getBeatsPerMinute();  // Calls function on our pulseSensor object that returns BPM as an "int".
+                                               // "myBPM" hold this BPM value now. 
+//  Serial.println(A HeartBeat Happened ! "); // If test is "true", print a message "a heartbeat happened".
+if (pulseSensor.sawStartOfBeat()) {            // Constantly test to see if "a beat happened".
+mybpm = pulseSensor.getBeatsPerMinute();  // Calls function on our pulseSensor object that returns BPM as an "int".
+                                               // "myBPM" hold this BPM value now. 
+//  Serial.println(A HeartBeat Happened ! "); // If test is "true", print a message "a heartbeat happened".
+}
+
+  delay(20);                    // considered best practice in a simple sketch.
+
+}
+                      // Print the value inside of myBPM. 
+
+
   for(int i=0;i<dSerial.available();i++)
   {
     char x =  dSerial.read();
@@ -63,7 +99,7 @@ void loop() {
     else if( x == 'x')
     {
       state = "x";
-     Serial.println(num);
+     //Serial.println(num);
       set_num();
     }
     else if(x == 'h')
@@ -120,6 +156,16 @@ void loop() {
   {
     state = "x";
   }
+  if(mybpm < 25)
+  {
+    state = "h";
+    printline="Need Help";
+    sSerial.listen();
+    SendMessage(num, "Need Help     ");
+    dSerial.listen();
+    started = true;
+    message_sent=1;
+  }
   // sSerial.listen();
 
   // if(sSerial.available())
@@ -140,4 +186,8 @@ void loop() {
   lcd.print(num);  
   lcd.setCursor(0,1);   //Move cursor to character 2 on line 1
   lcd.print(printline);
+  lcd.setCursor(10, 1);
+  lcd.print("BPM: ");
+  lcd.setCursor(14,1);
+  lcd.print(mybpm);
 }
